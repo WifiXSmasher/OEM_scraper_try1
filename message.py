139a4b6c11059a6ce102ac347ffdf42c1a632ea1
Ruchir .U. Kalla <164368036+WifiXSmasher@users.noreply.github.com>
2024-12-29 15:07:41 +0530

    Update message.py
    the password and email has been removed

--- message.py
+++ message.py
@@ -7,8 +7,8 @@ class Mail:
 
     @staticmethod
     def email(product, link, data):
-        sender = 'rk336@snu.edu.in'
-        PASSWORD = 'rtng osuu gyrt ifaa'
+        sender = 'Email here*'
+        PASSWORD = 'Password Here*'
         receiver = 'ruchirkalla007@gmail.com'
 
         subject = "new vulnerability found !"
